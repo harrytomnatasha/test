9c2e129dba3e07b8ebbe4d312d5172ce93116bb9
harrytomnatasha <39184679+harrytomnatasha@users.noreply.github.com>
2018-05-11 03:36:21 -0500

    Add files via upload

---
+++ abc.sh
@@ -0,0 +1,13 @@
+#alias c='rht-vmctl reset classroom'
+#alias s='rht-vmctl reset server'
+#alias d='rht-vmctl reset desktop'
+#rht-vmctl reset classroom
+#rht-vmctl reset server
+#rht-vmctl reset desktop
+#star 
+#for a in  1..5
+#do
+ssh -X root@192.168.4.7
+#don
+shift+ctrl+t
+ssh -X root@192.168.4.207
